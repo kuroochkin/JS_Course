0581ac1cc62dba7c1a69ec95902ff1a69229605d
Vlad <pin11kurochkin@gmail.com>
2023-03-15 02:43:14 +0300

    Циклы

---
+++ js/cycle.js
@@ -0,0 +1,19 @@
+let num1 = 50;
+
+while (num1 < 55)
+{
+    console.log(num1);
+    num1++;
+}
+
+let num2 = 50;
+
+do{
+    console.log(num2);
+    num2++;
+}
+while (num2 < 55);
+
+for (let i = 1; i < 8; i++){
+    console.log(i);
+}
