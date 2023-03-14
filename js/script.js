4913be5836ef1e0ee0b75704aa3921039de852e7
Vlad <pin11kurochkin@gmail.com>
2023-03-15 01:58:29 +0300

    Начало курса

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// const result = confirm("Ты тут?");
+
+const answer = [];
+
+answer[0] = prompt("Как дела?");
+answer[1] = prompt("Ты кто?");
+answer[2] = prompt("Сколько лет?");
+
+document.write(answer);
+
+
+
+
+
+
