8f6f068728635441bc7740b1cbb7b4ab8ca28b49
Vlad <pin11kurochkin@gmail.com>
2023-03-15 03:23:36 +0300

    Среднее дз по циклам

--- js/cycle.js
+++ js/cycle.js
@@ -21,12 +21,51 @@ for (let i = 2; i <= 10; i++ )
     console.log(i);
 }
 
-let arr = {};
+const arr = [];
 
-for (let i = 0; i <= 5; i++)
-{
+for (let i = 0; i <= 5; i++){
     arr[i] = i+5;
 } 
 
 console.log(arr);
+
+const result = [];
+
+for(let i = 0; i < arr.length; i++){
+    result[i] = arr[i];
+}
+
+console.log("Массив arr: " + arr);
+console.log("Массив result: " + result);
+
+
+let array = [2,5,6,7, "Bob"];
+
+for(let i = 0; i < array.length; i++)
+{
+    if(typeof(array[i]) === "number"){
+        array[i] = array[i]*2;
+    }
+    if(typeof(array[i]) === "string"){
+        array[i] += " - done";
+    }
+}
+
+console.log(array);
+
+let res = [];
+for (let i = 0; i < array.length; i++)
+{
+    if(typeof(array[i]) === "number"){
+        res[i] = array[i]/2;
+    }
+    if(typeof(array[i]) === "string"){
+        res[i] += " Bob";
+    }
+}
+
+console.log(res);
+
+
+
 
