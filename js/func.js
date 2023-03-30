de64d76e4ae5f19b10d135e5d286dd8f8980534e
Vlad <pin11kurochkin@gmail.com>
2023-03-30 14:13:29 +0300

    Домашнаяя работа с функциями

--- js/func.js
+++ js/func.js
@@ -39,5 +39,32 @@ function PrintResult(base, count){
 
 console.log(PrintResult(3, 10));
 
+function calculateVolumeAndArea(value){
+    if(value < 0 || typeof(value) != 'number'){
+        return "Произошла ошибка!";
+    }
+    else{
+        V = Math.pow(value, 3);
+        S = Math.pow(value, 2) * 6;
+
+        console.log(`Объем куба: ${V}`);
+        console.log(`Площадь поверхности куба: ${S}`);
+
+    }
+}
+
+calculateVolumeAndArea(5);
+
+function getCoupeNumber(value){
+    if(value <= 0 || value > 36 || typeof(value) != 'number'){
+        return "Произошла ошибка!";
+    }
+    else{
+        return Math.ceil(value / 4);
+    }
+}
+
+console.log(getCoupeNumber(10));
+
 
 
