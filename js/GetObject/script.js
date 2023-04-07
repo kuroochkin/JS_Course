40437cd39650868683997e170aaadf2ad812093e
Vlad <pin11kurochkin@gmail.com>
2023-04-07 16:51:07 +0300

    Работа в реальном проекте

--- js/GetObject/script.js
+++ js/GetObject/script.js
@@ -1,19 +1,32 @@
-const box = document.getElementById('box');
-
-console.log(box);
-
-const btns = document.getElementsByTagName('button')[1];
-
-console.log(btns);
-
-const circles = document.getElementsByClassName('circle');
-
-console.log(circles);
-
-const hearts = document.querySelectorAll('.heart');
-
-console.log(hearts);
-
+const box = document.getElementById('box'),
+        btns = document.getElementsByTagName('button')[1],
+        circles = document.getElementsByClassName('circle'),
+        hearts = document.querySelectorAll('.heart'),
+        wrapper = document.querySelector('.wrapper');
+        
 hearts.forEach(item => {
     console.log(item);
 });
+
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
+
+// for(let i = 0; i < hearts.length; i++){
+//     hearts[i].style.backgroundColor = 'blue';
+// }
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'green';
+});
+
+const div = document.createElement('div');
+const text  = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+wrapper.prepend(div);
+
+circles[0].remove();
+
+div.innerHTML = "Hello World!";
+
